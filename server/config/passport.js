49ee07f50867954bb83718bf24bb595b0854b859
KDS-7Git <kdshacker359@gmail.com>
2025-10-14 09:23:06 +0530

    Added Print Reciept

--- server/config/passport.js
+++ server/config/passport.js
@@ -10,9 +10,6 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
-// console.log('Google Client ID:', process.env.GOOGLE_CLIENT_ID);
-// console.log('Google Client Secret:', process.env.GOOGLE_CLIENT_SECRET ? 'Present' : 'Missing');
-
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
